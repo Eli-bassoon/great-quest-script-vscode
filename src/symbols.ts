c28761ec44a7da1db54f338406f7a5b496ed00e7
Eli-bassoon <61474669+Eli-bassoon@users.noreply.github.com>
2026-02-05 13:33:20 -0500

    Fix symbol breadcrumbs not showing for cursor position

--- src/symbols.ts
+++ src/symbols.ts
@@ -21,7 +21,7 @@ export class GQSDocumentSymbolProvider implements vscode.DocumentSymbolProvider 
             // Pop to stack if we are lower depth
             if (section.depth <= stack.length) {
                 while (stack.length && section.depth <= stack.length) {
-                    popStack(stack, symbols);
+                    popStack(stack, symbols, section.line);
                 }
             }
             // Depth must either increase by exactly one or decrease by any amount
@@ -32,24 +32,25 @@ export class GQSDocumentSymbolProvider implements vscode.DocumentSymbolProvider 
 
             // Push the new symbol
             const symbolKind = getSymbolKind(section.depth, section.name);
-
-            const range = new vscode.Range(section.line, section.match.indices.groups.name[0], section.line, section.match.indices.groups.name[1]);
-            const symbol = new vscode.DocumentSymbol(section.name, "", symbolKind, range, range);
+            const range = new vscode.Range(section.line, section.match.indices.groups.brackets1[0], section.line, section.match.indices.groups.brackets2[1]);
+            const selectionRange = new vscode.Range(section.line, section.match.indices.groups.name[0], section.line, section.match.indices.groups.name[1]);
+            const symbol = new vscode.DocumentSymbol(section.name, "", symbolKind, range, selectionRange);
 
             stack.push(symbol);
         }
 
         // EOF, pop all remaining ranges
         while (stack.length) {
-            popStack(stack, symbols);
+            popStack(stack, symbols, document.lineCount-1);
         }
 
         return symbols;
     }
 }
 
-function popStack(stack: vscode.DocumentSymbol[], symbols: vscode.DocumentSymbol[]) {
+function popStack(stack: vscode.DocumentSymbol[], symbols: vscode.DocumentSymbol[], line: number) {
     const child = stack.pop() as vscode.DocumentSymbol;
+    child.range = new vscode.Range(child.range.start, new vscode.Position(line, 0));
     if (stack.length) {
         stack.at(-1)?.children.push(child);
     }
@@ -74,11 +75,8 @@ function getSymbolKind(depth: number, name: string): vscode.SymbolKind {
                 case "Script":
                     return vscode.SymbolKind.Namespace;
 
-                case "Function":
-                    return vscode.SymbolKind.Function;
-
                 default:
-                    return vscode.SymbolKind.Variable;
+                    return vscode.SymbolKind.Function;
             }
 
         // Fourth-level is always a function
